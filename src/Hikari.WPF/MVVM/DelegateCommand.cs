b04c52a7855e4ae069bd9e8e95d13f439998b24e
子不语 <xmh.yxw@gmail.com>
2021-05-21 08:48:37 +0800

    添加了一个wpfmvvm库

---
+++ src/Hikari.WPF/MVVM/DelegateCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Hikari.WPF.MVVM
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> _executeMethod;
+        private readonly Func<T, bool> _canExecuteMethod;
+
+        public DelegateCommand(Action<T> executeMethod)
+            : this(executeMethod, null)
+        { }
+
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod ?? throw new ArgumentNullException(nameof(executeMethod));
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        #region ICommand 成员
+        /// <summary>
+        ///  Method to determine if the command can be executed
+        /// </summary>
+        public bool CanExecute(T parameter)
+        {
+            if (_canExecuteMethod != null)
+            {
+                return _canExecuteMethod(parameter);
+            }
+            return true;
+
+        }
+
+        /// <summary>
+        ///  Execution of the command
+        /// </summary>
+        public void Execute(T parameter)
+        {
+            _executeMethod?.Invoke(parameter);
+        }
+
+        #endregion
+
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        #region ICommand 成员
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter == null && typeof(T).IsValueType)
+            {
+                return (_canExecuteMethod == null);
+
+            }
+
+            return CanExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Hikari.WPF/MVVM/NotificationObject.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hikari.WPF.MVVM
+{
+    /// <summary>
+    /// INotifyPropertyChanged 用于通知属性改变（实现ViewModel向View喊话，所有绑定该属性的都会得到通知）
+    /// </summary>
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// 属性改变时调用该方法发出通知
+        /// </summary>
+        /// <param name="propertyName">[CallerMemberName] 是.net 4.5的新特性，可获取调用者的名称</param>
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

fab32611ad64f3450999966dba4f1a01e26a02a7
子不语 <xmh.yxw@gmail.com>
2021-10-12 16:32:13 +0800

    升级了版本，添加了描述文件

--- src/Hikari.Common/LogHelper.cs
+++ src/Hikari.Common/LogHelper.cs
@@ -1,6 +1,4 @@
-﻿
-
-/******************************************************************************************************************
+﻿/******************************************************************************************************************
  * 
  * 
  * 说　明： 错误日志输出类(版本：Version1.0.0)
@@ -13,8 +11,6 @@
  * 
  * ***************************************************************************************************************/
 
-using System;
-
 namespace Hikari.Common
 {
     /// <summary>
@@ -22,10 +18,10 @@ namespace Hikari.Common
     /// </summary>
     public class LogHelper
     {
-        private static readonly object Locker = new object();
+        private static readonly object _locker = new object();
 
-        private static string LoginfoDir => GetLogger("loginfo");
-        private static string LogerrorDir => GetLogger("logerror");
+        private static string _loginfoDir => GetLogger("loginfo");
+        private static string _logerrorDir => GetLogger("logerror");
 
         /// <summary>
         /// 输出log
@@ -33,27 +29,15 @@ namespace Hikari.Common
         /// <param name="info">消息</param>
         public static void WriteLog(string info)
         {
-            lock (Locker)
+            lock (_locker)
             {
-                System.IO.StreamWriter sw = null;
-                try
-                {
-                    string directPath = System.IO.Path.Combine(LoginfoDir, $"{DateTime.Now:yyyy-MM-dd}_LogInfo.log");
-                    sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);    //判断文件是否存在如果不存在则创建，如果存在则添加。
+                string directPath = System.IO.Path.Combine(_loginfoDir, $"{DateTime.Now:yyyy-MM-dd}_LogInfo.log");
+                using System.IO.StreamWriter sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);    //判断文件是否存在如果不存在则创建，如果存在则添加。
 
-                    //把信息输出到文件
-                    sw.WriteLine(info);
-                    sw.WriteLine("***********************************************************************");
-                    sw.WriteLine();
-                }
-                finally
-                {
-                    if (sw != null)
-                    {
-                        sw.Flush();
-                        sw.Dispose();
-                    }
-                }
+                //把信息输出到文件
+                sw.WriteLine(info);
+                sw.WriteLine("***********************************************************************");
+                sw.WriteLine();
             }
         }
 
@@ -63,32 +47,20 @@ namespace Hikari.Common
         /// <param name="ex">异常</param>
         public static void WriteError(Exception ex)
         {
-            lock (Locker)
+            lock (_locker)
             {
-                System.IO.StreamWriter sw = null;
-                try
-                {
-                    string directPath = System.IO.Path.Combine(LogerrorDir, $"{DateTime.Now:yyyy-MM-dd}_LogError.log");
+                string directPath = System.IO.Path.Combine(_logerrorDir, $"{DateTime.Now:yyyy-MM-dd}_LogError.log");
 
-                    sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);    //判断文件是否存在如果不存在则创建，如果存在则添加。
+                using System.IO.StreamWriter sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);    //判断文件是否存在如果不存在则创建，如果存在则添加。
 
-                    //把异常信息输出到文件
-                    sw.WriteLine("当前时间：" + DateTime.Now);
-                    sw.WriteLine("异常信息：" + ex.Message);
-                    sw.WriteLine("异常对象：" + ex.Source);
-                    sw.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
-                    sw.WriteLine("触发方法：" + ex.TargetSite);
-                    sw.WriteLine("***********************************************************************");
-                    sw.WriteLine();
-                }
-                finally
-                {
-                    if (sw != null)
-                    {
-                        sw.Flush();
-                        sw.Dispose();
-                    }
-                }
+                //把异常信息输出到文件
+                sw.WriteLine("当前时间：" + DateTime.Now);
+                sw.WriteLine("异常信息：" + ex.Message);
+                sw.WriteLine("异常对象：" + ex.Source);
+                sw.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
+                sw.WriteLine("触发方法：" + ex.TargetSite);
+                sw.WriteLine("***********************************************************************");
+                sw.WriteLine();
             }
 
         }
@@ -99,33 +71,20 @@ namespace Hikari.Common
         /// <param name="tag">传入标签（这里用于标识函数由哪个线程调用）</param>
         public static void WriteError(Exception ex, string tag)
         {
-            lock (Locker)
+            lock (_locker)
             {
-                System.IO.StreamWriter sw = null;
-                try
-                {
+                string directPath = System.IO.Path.Combine(_logerrorDir, $"{DateTime.Now:yyyy-MM-dd}_LogError.log");
 
-                    string directPath = System.IO.Path.Combine(LogerrorDir, $"{DateTime.Now:yyyy-MM-dd}_LogError.log");
+                using System.IO.StreamWriter sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);//判断文件是否存在如果不存在则创建，如果存在则添加。
 
-                    sw = !System.IO.File.Exists(directPath) ? System.IO.File.CreateText(directPath) : System.IO.File.AppendText(directPath);//判断文件是否存在如果不存在则创建，如果存在则添加。
-
-                    //把异常信息输出到文件
-                    sw.WriteLine(string.Concat('[', DateTime.Now, "] Tag:" + tag));
-                    sw.WriteLine("异常信息：" + ex.Message);
-                    sw.WriteLine("异常对象：" + ex.Source);
-                    sw.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
-                    sw.WriteLine("触发方法：" + ex.TargetSite);
-                    sw.WriteLine("***********************************************************************");
-                    sw.WriteLine();
-                }
-                finally
-                {
-                    if (sw != null)
-                    {
-                        sw.Flush();
-                        sw.Dispose();
-                    }
-                }
+                //把异常信息输出到文件
+                sw.WriteLine(string.Concat('[', DateTime.Now, "] Tag:" + tag));
+                sw.WriteLine("异常信息：" + ex.Message);
+                sw.WriteLine("异常对象：" + ex.Source);
+                sw.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
+                sw.WriteLine("触发方法：" + ex.TargetSite);
+                sw.WriteLine("***********************************************************************");
+                sw.WriteLine();
             }
         }
         /// <summary>
@@ -138,11 +97,11 @@ namespace Hikari.Common
             string logDir;
             if (logType == "loginfo")
             {
-                logDir = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Logs", "LogInfo");  //如果日志文件为空，则默认在Debug目录下新建 Log\LogInfo目录
+                logDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "LogInfo");  //如果日志文件为空，则默认在Debug目录下新建 Log\LogInfo目录
             }
             else
             {
-                logDir = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Logs", "LogError");  //如果日志文件为空，则默认在Debug目录下新建 Log\LogError目录
+                logDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "LogError");  //如果日志文件为空，则默认在Debug目录下新建 Log\LogError目录
             }
 
             if (!System.IO.Directory.Exists(logDir))   //判断文件夹是否存在，如果不存在则创建

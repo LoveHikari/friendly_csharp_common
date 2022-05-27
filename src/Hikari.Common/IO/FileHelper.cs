1333c46d46af50743bb722542f1d2f7fc1911689
LoveHikari <xmh.yxw@gmail.com>
2022-05-27 18:35:34 +0800

    新增了几个同步方法

--- src/Hikari.Common/IO/FileHelper.cs
+++ src/Hikari.Common/IO/FileHelper.cs
@@ -32,7 +32,19 @@ public class FileHelper
         await using System.IO.StreamWriter f2 = new System.IO.StreamWriter(path, false, System.Text.Encoding.GetEncoding(charset));
         await f2.WriteAsync(strings);
     }
+    /// <summary>
+    /// 写文件，如果文件不存在则创建，存在则覆盖
+    /// </summary>
+    /// <param name="path">文件路径</param>
+    /// <param name="strings">文件内容</param>
+    /// <param name="charset">编码，默认utf-8</param>
+    public static void Write(string path, string strings, string charset = "utf-8")
+    {
+        Create(path);
 
+        using System.IO.StreamWriter f2 = new System.IO.StreamWriter(path, false, System.Text.Encoding.GetEncoding(charset));
+        f2.Write(strings);
+    }
     /// <summary>
     /// 写文件，如果文件不存在则创建，存在则追加
     /// </summary>
@@ -45,6 +57,19 @@ public class FileHelper
 
         await using System.IO.StreamWriter f2 = new System.IO.StreamWriter(path, true, System.Text.Encoding.GetEncoding(charset));
         await f2.WriteAsync(strings);
+    }
+    /// <summary>
+    /// 写文件，如果文件不存在则创建，存在则追加
+    /// </summary>
+    /// <param name="path">文件路径</param>
+    /// <param name="strings">内容</param>
+    /// <param name="charset">编码，默认utf-8</param>
+    public static void Append(string path, string strings, string charset = "utf-8")
+    {
+        Create(path);
+
+        using System.IO.StreamWriter f2 = new System.IO.StreamWriter(path, true, System.Text.Encoding.GetEncoding(charset));
+        f2.Write(strings);
     }
     /// <summary>
     ///  将 Stream 写入文件 
@@ -65,6 +90,25 @@ public class FileHelper
         await using BinaryWriter bw = new BinaryWriter(fs);
         bw.Write(bytes);
     }
+    /// <summary>
+    ///  将 Stream 写入文件 
+    /// </summary>
+    /// <param name="filePath">文件路径</param>
+    /// <param name="stream"></param>
+    public static void Write(string filePath, Stream stream)
+    {
+        Create(filePath);
+
+        // 把 Stream 转换成 byte[] 
+        byte[] bytes = new byte[stream.Length];
+        stream.Read(bytes, 0, bytes.Length);
+        // 设置当前流的位置为流的开始 
+        stream.Seek(0, SeekOrigin.Begin);
+        // 把 byte[] 写入文件 
+        using FileStream fs = new FileStream(filePath, FileMode.Create);
+        using BinaryWriter bw = new BinaryWriter(fs);
+        bw.Write(bytes);
+    }
     #endregion
 
     #region 读文件
@@ -82,6 +126,18 @@ public class FileHelper
         return s;
     }
     /// <summary>
+    /// 读文件
+    /// </summary>
+    /// <param name="path">文件路径</param>
+    /// <param name="charset">编码，默认utf-8</param>
+    /// <returns></returns>
+    public static string Read(string path, string charset = "utf-8")
+    {
+        using System.IO.StreamReader f2 = new System.IO.StreamReader(path, System.Text.Encoding.GetEncoding(charset));
+        var s = f2.ReadToEnd();
+        return s;
+    }
+    /// <summary>
     /// 从文件读取 Stream 
     /// </summary>
     /// <param name="filePath"></param>
@@ -95,6 +151,22 @@ public class FileHelper
         await fileStream.ReadAsync(bytes, 0, bytes.Length);
         // 把 byte[] 转换成 Stream 
         await using Stream stream = new MemoryStream(bytes);
+        return stream;
+    }
+    /// <summary>
+    /// 从文件读取 Stream 
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public static Stream ReadFile(string filePath)
+    {
+        // 打开文件 
+        using FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        // 读取文件的 byte[] 
+        byte[] bytes = new byte[fileStream.Length];
+        fileStream.Read(bytes, 0, bytes.Length);
+        // 把 byte[] 转换成 Stream 
+        using Stream stream = new MemoryStream(bytes);
         return stream;
     }
     #endregion
@@ -161,7 +233,7 @@ public class FileHelper
         fileName = NameProcess(fileName);
         string extension = System.IO.Path.GetExtension(filePath);
         string? fileDir = System.IO.Path.GetDirectoryName(filePath);
-        return System.IO.Path.Combine(fileDir??"", fileName + extension);
+        return System.IO.Path.Combine(fileDir ?? "", fileName + extension);
     }
     /// <summary>
     /// 文件名处理

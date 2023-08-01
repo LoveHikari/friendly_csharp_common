7bc3af97ec72d886f359344da9333814f051d99a
LoveHikari <xmh.yxw@gmail.com>
2023-08-01 11:22:43 +0800

    修复linq的and和or方法错误问题

--- src/Hikari.Common/PredicateExtensions.cs
+++ src/Hikari.Common/PredicateExtensions.cs
@@ -18,14 +18,16 @@ namespace Hikari.Common
         /// <returns></returns>
         public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> one, Expression<Func<T, bool>> another)
         {
-            var candidateExpr = Expression.Parameter(typeof(T), "candidate");
-            var parameterReplacer = new ParameterReplacer(candidateExpr);
+            //var candidateExpr = Expression.Parameter(typeof(T), "candidate");
+            //var parameterReplacer = new ParameterReplacer(candidateExpr);
 
-            var left = parameterReplacer.Replace(one.Body);
-            var right = parameterReplacer.Replace(another.Body);
-            var body = Expression.And(left, right);
+            //var left = parameterReplacer.Replace(one.Body);
+            //var right = parameterReplacer.Replace(another.Body);
+            //var body = Expression.AndAlso(left, right);
 
-            return Expression.Lambda<Func<T, bool>>(body, candidateExpr);
+            //return Expression.Lambda<Func<T, bool>>(body, candidateExpr);
+            var inv = Expression.Invoke(another, one.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(one.Body, inv), one.Parameters);
         }
         /// <summary>
         /// 或
@@ -36,33 +38,35 @@ namespace Hikari.Common
         /// <returns></returns>
         public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> one, Expression<Func<T, bool>> another)
         {
-            var candidateExpr = Expression.Parameter(typeof(T), "candidate");
-            var parameterReplacer = new ParameterReplacer(candidateExpr);
+            //var candidateExpr = Expression.Parameter(typeof(T), "candidate");
+            //var parameterReplacer = new ParameterReplacer(candidateExpr);
 
-            var left = parameterReplacer.Replace(one.Body);
-            var right = parameterReplacer.Replace(another.Body);
-            var body = Expression.Or(left, right);
+            //var left = parameterReplacer.Replace(one.Body);
+            //var right = parameterReplacer.Replace(another.Body);
+            //var body = Expression.OrElse(left, right);
 
-            return Expression.Lambda<Func<T, bool>>(body, candidateExpr);
+            //return Expression.Lambda<Func<T, bool>>(body, candidateExpr);
+            var invokedExpr = Expression.Invoke(another, one.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.OrElse(one.Body, invokedExpr), one.Parameters);
         }
-        internal class ParameterReplacer : ExpressionVisitor
-        {
-            public ParameterReplacer(ParameterExpression paramExpr)
-            {
-                this.ParameterExpression = paramExpr;
-            }
+        //internal class ParameterReplacer : ExpressionVisitor
+        //{
+        //    public ParameterReplacer(ParameterExpression paramExpr)
+        //    {
+        //        this.ParameterExpression = paramExpr;
+        //    }
 
-            public ParameterExpression ParameterExpression { get; private set; }
+        //    public ParameterExpression ParameterExpression { get; private set; }
 
-            public Expression Replace(Expression expr)
-            {
-                return this.Visit(expr);
-            }
+        //    public Expression Replace(Expression expr)
+        //    {
+        //        return this.Visit(expr);
+        //    }
 
-            protected override Expression VisitParameter(ParameterExpression p)
-            {
-                return this.ParameterExpression;
-            }
-        }
+        //    protected override Expression VisitParameter(ParameterExpression p)
+        //    {
+        //        return this.ParameterExpression;
+        //    }
+        //}
     }
 }

86b0c46e0ee8d7c4cf407eaa02ed9f21e397c227
LoveHikari <xmh.yxw@gmail.com>
2024-02-01 12:01:40 +0800

    更新

--- src/Hikari.Common.Web.AspNetCore/ConfigurationManager.cs
+++ src/Hikari.Common.Web.AspNetCore/ConfigurationManager.cs
@@ -1,67 +1,67 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+﻿//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.Json;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Options;
 
-namespace Hikari.Common.Web.AspNetCore
-{
-    /// <summary>
-    /// 配置管理器
-    /// <remarks>https://blog.csdn.net/hofmann/article/details/118565090</remarks>
-    /// </summary>
-    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never), System.ComponentModel.Browsable(false)]
-    public static class ConfigurationManager
-    {
-        private static IConfiguration _configuration;
-        static ConfigurationManager()
-        {
-            // Microsoft.Extensions.Configuration扩展包提供的
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(System.Environment.CurrentDirectory)
-                .Add(new JsonConfigurationSource { Path = "appsettings.json", Optional = false, ReloadOnChange = true })
-                .Add(new JsonConfigurationSource { Path = "appsettings.Development.json", Optional = true, ReloadOnChange = true });
+//namespace Hikari.Common.Web.AspNetCore
+//{
+//    /// <summary>
+//    /// 配置管理器
+//    /// <remarks>https://blog.csdn.net/hofmann/article/details/118565090</remarks>
+//    /// </summary>
+//    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never), System.ComponentModel.Browsable(false)]
+//    public static class ConfigurationManager
+//    {
+//        private static IConfiguration _configuration;
+//        static ConfigurationManager()
+//        {
+//            // Microsoft.Extensions.Configuration扩展包提供的
+//            var builder = new ConfigurationBuilder()
+//                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+//                .Add(new JsonConfigurationSource { Path = "appsettings.json", Optional = false, ReloadOnChange = true })
+//                .Add(new JsonConfigurationSource { Path = "appsettings.Development.json", Optional = true, ReloadOnChange = true });
 
-            _configuration = builder.Build();
+//            _configuration = builder.Build();
 
-        }
-        /// <summary>
-        /// 依赖注入添加配置
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="configuration"></param>
-        public static void AddConfiguration(this IServiceCollection services, IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-        /// <summary>
-        /// 获取应用设置
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public static T GetAppSettings<T>(string key) where T : class, new()
-        {
-            var appConfig = new ServiceCollection()
-                .AddOptions()
-                .Configure<T>(_configuration.GetSection(key))
-                .BuildServiceProvider()
-                .GetService<IOptions<T>>()
-                ?.Value;
+//        }
+//        /// <summary>
+//        /// 依赖注入添加配置
+//        /// </summary>
+//        /// <param name="services"></param>
+//        /// <param name="configuration"></param>
+//        public static void AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+//        {
+//            _configuration = configuration;
+//        }
+//        /// <summary>
+//        /// 获取应用设置
+//        /// </summary>
+//        /// <typeparam name="T"></typeparam>
+//        /// <param name="key"></param>
+//        /// <returns></returns>
+//        public static T GetAppSettings<T>(string key) where T : class, new()
+//        {
+//            var appConfig = new ServiceCollection()
+//                .AddOptions()
+//                .Configure<T>(_configuration.GetSection(key))
+//                .BuildServiceProvider()
+//                .GetService<IOptions<T>>()
+//                ?.Value;
 
-            return appConfig;
-        }
-        ///// <summary>
-        ///// 应用设置
-        ///// </summary>
-        //public static IConfiguration AppSettings => _configuration;
-        /// <summary>
-        /// 获取应用设置
-        /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public static string Get(string key)
-        {
-            return _configuration[key];
-        }
-    }
-}
+//            return appConfig;
+//        }
+//        ///// <summary>
+//        ///// 应用设置
+//        ///// </summary>
+//        //public static IConfiguration AppSettings => _configuration;
+//        /// <summary>
+//        /// 获取应用设置
+//        /// </summary>
+//        /// <param name="key"></param>
+//        /// <returns></returns>
+//        public static string Get(string key)
+//        {
+//            return _configuration[key];
+//        }
+//    }
+//}
--- src/Hikari.Common.Web.AspNetCore/Logging/LoggerFactoryExtensions.cs
+++ src/Hikari.Common.Web.AspNetCore/Logging/LoggerFactoryExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using NLog;
+using NLog.Web;
 
 namespace Hikari.Common.Web.AspNetCore.Logging
 {
@@ -12,11 +14,11 @@ namespace Hikari.Common.Web.AspNetCore.Logging
         /// 添加nlog
         /// </summary>
         /// <param name="app"></param>
-        /// <param name="nlogName">nlog文件名字</param>
-        public static void UseNlog(this IApplicationBuilder app, string nlogName = "nlog.config")
+        public static void UseNlog(this IApplicationBuilder app)
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);//这是为了防止中文乱码
-            NLog.Web.NLogBuilder.ConfigureNLog(System.IO.Path.Combine(System.Environment.CurrentDirectory, nlogName));//读取Nlog配置文件
+            // NLog.Web.NLogBuilder.ConfigureNLog(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nlogName));//读取Nlog配置文件
+            NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();//读取Nlog配置文件
         }
     }
 }

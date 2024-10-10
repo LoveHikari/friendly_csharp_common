1d642741c1d72bb875636f225d301de13ec83b6f
LoveHikari <xmh.yxw@gmail.com>
2024-10-10 08:10:28 +0800

    update

--- src/Hikari.Mvvm/Command/DelegateCommand.cs
+++ src/Hikari.Mvvm/Command/DelegateCommand.cs
@@ -1,72 +1,82 @@
 ﻿using System;
 using System.Windows.Input;
 
-namespace Hikari.Mvvm.Command
+namespace Hikari.Mvvm.Command;
+public class DelegateCommand<T> : ICommand
 {
-    public class DelegateCommand<T> : ICommand
+    private readonly Action<T> _executeMethod;
+    private readonly Func<T, bool> _canExecuteMethod;
+
+    public DelegateCommand(Action<T> executeMethod)
+        : this(executeMethod, null)
+    { }
+
+    public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
     {
-        private readonly Action<T> _executeMethod;
-        private readonly Func<T, bool> _canExecuteMethod;
+        _executeMethod = executeMethod ?? throw new ArgumentNullException(nameof(executeMethod));
+        _canExecuteMethod = canExecuteMethod;
+    }
 
-        public DelegateCommand(Action<T> executeMethod)
-            : this(executeMethod, null)
-        { }
-
-        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+    #region ICommand 成员
+    /// <summary>
+    ///  Method to determine if the command can be executed
+    /// </summary>
+    public bool CanExecute(T parameter)
+    {
+        if (_canExecuteMethod != null)
         {
-            _executeMethod = executeMethod ?? throw new ArgumentNullException(nameof(executeMethod));
-            _canExecuteMethod = canExecuteMethod;
+            return _canExecuteMethod(parameter);
+        }
+        return true;
+
+    }
+
+    /// <summary>
+    ///  Execution of the command
+    /// </summary>
+    public void Execute(T parameter)
+    {
+        _executeMethod?.Invoke(parameter);
+    }
+
+    #endregion
+
+
+    event EventHandler ICommand.CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    #region ICommand 成员
+
+    public bool CanExecute(object parameter)
+    {
+        if (parameter == null && typeof(T).IsValueType)
+        {
+            return (_canExecuteMethod == null);
+
         }
 
-        #region ICommand 成员
-        /// <summary>
-        ///  Method to determine if the command can be executed
-        /// </summary>
-        public bool CanExecute(T parameter)
-        {
-            if (_canExecuteMethod != null)
-            {
-                return _canExecuteMethod(parameter);
-            }
-            return true;
+        return CanExecute((T)parameter);
+    }
 
-        }
+    public void Execute(object parameter)
+    {
+        Execute((T)parameter);
+    }
 
-        /// <summary>
-        ///  Execution of the command
-        /// </summary>
-        public void Execute(T parameter)
-        {
-            _executeMethod?.Invoke(parameter);
-        }
-
-        #endregion
+    #endregion
+}
 
 
-        event EventHandler ICommand.CanExecuteChanged
-        {
-            add => CommandManager.RequerySuggested += value;
-            remove => CommandManager.RequerySuggested -= value;
-        }
+public class DelegateCommand : DelegateCommand<object>
+{
+    public DelegateCommand(Action<object> executeMethod) : base(executeMethod)
+    {
+    }
 
-        #region ICommand 成员
-
-        public bool CanExecute(object parameter)
-        {
-            if (parameter == null && typeof(T).IsValueType)
-            {
-                return (_canExecuteMethod == null);
-
-            }
-
-            return CanExecute((T)parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            Execute((T)parameter);
-        }
-
-        #endregion
+    public DelegateCommand(Action<object> executeMethod, Func<object, bool> canExecuteMethod) : base(executeMethod, canExecuteMethod)
+    {
     }
 }

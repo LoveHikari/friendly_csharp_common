7c02147a00c9e27be018fd6d738f7e3fcbe7fcbc
LoveHikari <xmh.yxw@gmail.com>
2024-11-25 10:04:08 +0800

    update

---
+++ src/Hikari.Mvvm/Converter/VisibilityToBooleanConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Hikari.Mvvm.Converter
+{
+    [ValueConversion(typeof(Visibility), typeof(bool))]
+    public class VisibilityToBooleanConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility intValue)
+            {
+                return intValue == Visibility.Visible;
+            }
+            return false;
+
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+
+        }
+    }
+}

275906e92552c8e1f56c290a1981957953bafa9a
LoveHikari <xmh.yxw@gmail.com>
2024-12-18 16:13:39 +0800

    add StringToBooleanConverter

---
+++ src/Hikari.Mvvm/Converter/StringToBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Hikari.Mvvm.Converter
+{
+    /// <summary>
+    /// ConverterParameter与原值相同时为true
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class StringToBooleanConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (value.ToString()?.Equals(parameter) ?? false);
+
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
